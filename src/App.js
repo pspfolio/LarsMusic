3cc8441e6fe568bb49db5ef75b051e7e7c2a5387
Petteri Suominen <pspfolio@gmail.com>
2018-04-30 17:23:41 +0300

    adding prettier husky

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  test = () => {};
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
