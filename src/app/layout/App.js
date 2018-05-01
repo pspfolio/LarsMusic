ac91618dc5d236f2e6e85af1fbb987edd1e2f836
Petteri Suominen <pspfolio@gmail.com>
2018-05-01 10:31:42 +0300

    adding login component

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import Loading from 'common/components/Loading';
+
 import './App.css';
 
-class App extends Component {
-  test = () => {};
+const AsyncLoginComponent = Loadable({
+  loader: () => import('features/login/Login'),
+  loading: Loading
+});
 
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/login" component={AsyncLoginComponent} />
+          <Route render={() => <h1>Four oh Four</h1>} />
+        </Switch>
+      </Router>
     );
   }
 }
