c34993ff5789428c97de907c5b9c46259b54f581
Petteri Suominen <pspfolio@gmail.com>
2018-05-16 21:57:24 +0300

    styling top tracks list

--- src/features/tracks/topTracks/TopTracks.jsx
+++ src/features/tracks/topTracks/TopTracks.jsx
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 import { fetchTopTracksIfNeeded } from '../tracksActions';
 import { selectTopTracksByArtistId } from '../tracksSelectors';
+
+const TopTracksCard = styled.article`
+  margin: 32px;
+  max-width: 60%;
+`;
+
+const Title = styled.h5`
+  font-size: 24px;
+  letter-spacing: 0.25px;
+`;
+
+const TrackList = styled.ul`
+  margin-left: 0;
+  padding-left: 0;
+`;
+
+const TrackListRow = styled.li`
+  margin-top: 16px;
+  display: flex;
+`;
+
+const TrackListImage = styled.img`
+  height: 64px;
+`;
+
+const TrackListNameWrapper = styled.div`
+  margin-left: 16px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-evenly;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const TrackListName = styled.span`
+  font-size: 16px;
+  letter-spacing: 0.5px;
+  font-weight: 500;
+`;
+
+const TrackListAlbumName = styled.span`
+  display: block;
+  font-size: 14px;
+  letter-spacing: 0.25px;
+  color: rgba(0, 0, 0, 0.6);
+`;
 
 class TopTracks extends Component {
   componentDidMount() {
@@ -13,29 +62,37 @@ class TopTracks extends Component {
   render() {
     const { topTracks } = this.props;
     return (
-      <article>
+      <TopTracksCard>
+        <Title>Top Tracks</Title>
         {topTracks && (
-          <ul>
+          <TrackList>
             {topTracks.map(track => (
-              <li key={track.id}>
-                <img src={track.album.images.find(img => img.height < 100).url} alt={`${track.name} album art`} />
-                <h4>{track.name}</h4>
-                <h4>{track.album.name}</h4>
+              <TrackListRow key={track.id}>
+                <TrackListImage
+                  src={track.album.images.find(img => img.height < 100).url}
+                  alt={`${track.name} album art`}
+                />
+                <TrackListNameWrapper>
+                  <TrackListName>{track.name}</TrackListName>
+                  <TrackListAlbumName>{track.album.name}</TrackListAlbumName>
+                </TrackListNameWrapper>
+
                 <a href={track.external_urls['spotify']} target="_blank">
                   Open in Spotify
                 </a>
-              </li>
+              </TrackListRow>
             ))}
-          </ul>
+          </TrackList>
         )}
-      </article>
+      </TopTracksCard>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  const limit = 5;
   return {
-    topTracks: selectTopTracksByArtistId(state, ownProps.match.params.id)
+    topTracks: selectTopTracksByArtistId(state, ownProps.match.params.id, limit)
   };
 };
 
--- src/features/tracks/tracksSelectors.js
+++ src/features/tracks/tracksSelectors.js
@@ -2,8 +2,9 @@ import { createSelector } from 'reselect';
 
 export const getTopTracks = state => state.track.topTracks;
 
-const getTopTracksById = (state, id) => {
-  return state.track.topTracks[id];
+const getTopTracksById = (state, id, limit) => {
+  const topTracksById = state.track.topTracks[id];
+  return topTracksById ? topTracksById.slice(0, limit) : topTracksById;
 };
 
 export const selectTopTracksByArtistId = createSelector(getTopTracksById, tracks => tracks);
