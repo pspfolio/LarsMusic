7823d5293a3545cf519dd14a75a16aa6d1c91c5e
Petteri Suominen <pspfolio@gmail.com>
2018-05-25 22:32:14 +0300

    styling play button for player

--- src/features/play/Play/Play.jsx
+++ src/features/play/Play/Play.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { selectPlayingArtists, selectPlayingTrack } from '../playSelectors';
+import play_icon from 'assets/images/play_icon.svg';
 
 const PlayContainer = styled.div`
   position: fixed;
@@ -11,56 +12,92 @@ const PlayContainer = styled.div`
   right: 0;
   height: 80px;
   background-color: rgba(255, 255, 255, 0.95);
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 const TrackName = styled.p`
-  align-self: center;
-  margin: 0 32px;
-  height: 100%;
   font-weight: 500;
   letter-spacing: 0.25px;
   color: rgba(0, 0, 0, 0.87);
+`;
+
+const ActionButtons = styled.div``;
+
+const PlayIcon = styled.img`
+  height: 24px;
+`;
+
+const PlayButton = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 2px solid rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  border-radius: 500%;
+  cursor: pointer;
+  will-change: transform;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 7px 10px rgba(50, 50, 93, 0.1);
+    transform: translateY(-2px);
+  }
 `;
 
 class Play extends Component {
   constructor(props) {
     super(props);
     this.audio = React.createRef();
+
+    this.state = {
+      playing: true
+    };
   }
   componentDidMount() {
-    console.log(this.audio.current);
     this.audio.current && this.audio.current.load();
-    //this.audio.load();
   }
 
-  shouldComponentUpdate(nextProps) {
-    console.log(nextProps);
-    return nextProps.playingTrack.name !== this.props.playingTrack.name;
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { playing: true };
   }
 
   play = () => {
+    this.setState({ playing: true });
     this.audio.current.play();
   };
 
   pause = () => {
+    this.setState({ playing: false }, () => {
+      console.log('state changte');
+    });
     this.audio.current.pause();
   };
 
   render() {
+    const { playing } = this.state;
     const { playingArtist, playingTrack } = this.props;
-    this.audio.current && this.audio.current.load();
+    console.log(playing);
 
     return (
       <div>
         {playingArtist && playingTrack ? (
           <PlayContainer>
             <TrackName>{playingTrack.name}</TrackName>
-            <button type="button" onClick={this.play}>
-              Play
-            </button>
-            <button type="button" onClick={this.pause}>
-              Pause
-            </button>
+            <ActionButtons>
+              {!playing ? (
+                <PlayButton type="button" onClick={this.play}>
+                  <PlayIcon src={play_icon} />
+                </PlayButton>
+              ) : (
+                <button type="button" onClick={this.pause}>
+                  Pause
+                </button>
+              )}
+            </ActionButtons>
+            <div>Volume</div>
             <audio autoPlay src={playingTrack.preview_url} ref={this.audio} />
           </PlayContainer>
         ) : null}
