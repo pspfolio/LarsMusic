5f21623cc25a7e4d0d0cdbe646eb4944c9e486e1
Petteri Suominen <pspfolio@gmail.com>
2018-06-02 09:41:38 +0300

    styling input field

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -10,7 +10,6 @@ import './App.css';
 
 const FullViewHeight = styled.div`
   height: 100vh;
-  font-family: 'Roboto', sans-serif;
   background-color: #f8f8f8;
 `;
 
--- src/features/search/Search.jsx
+++ src/features/search/Search.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import styled from 'styled-components';
+import DashboardLayout from 'features/dashboard//DashboardLayout';
+
+const FormWrapper = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-top: 32px;
+`;
+
+const SearchInput = styled(Field)`
+  font-family: 'Roboto';
+  height: 32px;
+  font-size: 18px;
+  letter-spacing: 0.8px;
+  padding: 12px 16px;
+  border-radius: 8px;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.86);
+  width: 40%;
+  box-shadow: 0px 5px 20px 2px rgba(0, 0, 0, 0.03);
+  -webkit-appearance: none;
+  border: 0;
+  outline: 0;
+
+  &::-webkit-input-placeholder {
+    color: #caced2;
+  }
+  &::-moz-placeholder {
+    color: #caced2;
+  }
+  &:-ms-input-placeholder {
+    color: #caced2;
+  }
+  &:-moz-placeholder {
+    color: #caced2;
+  }
+`;
 
 const Search = props => {
   return (
-    <form onSubmit={() => {}}>
-      <label htmlFor="search">Search</label>
-      <Field name="search" component="input" type="text" />
-    </form>
+    <DashboardLayout>
+      <FormWrapper onSubmit={() => {}}>
+        <SearchInput name="search" component="input" type="text" placeholder="Search Artist" />
+      </FormWrapper>
+    </DashboardLayout>
   );
 };
 
