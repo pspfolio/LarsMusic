392dfb6ff4d58ffaff10fd0f5c77d425351f744c
Petteri Suominen <pspfolio@gmail.com>
2018-06-06 21:10:26 +0300

    styling tab links

--- src/features/tabs/Tab.jsx
+++ src/features/tabs/Tab.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TabWrapper = styled.div`
+  margin-right: 72px;
+  font-size: ${props => (props.active ? '30px' : '20px')};
+  color: ${props => (props.active ? '#76ACFC' : '#cfcfcf')};
+  font-weight: ${props => (props.active ? 500 : 400)};
+  position: relative;
+  cursor: pointer;
+  transition: color 0.3s ease-out;
+
+  &:hover {
+    color: #76acfc;
+  }
+
+  &::after {
+    content: '';
+    display: ${props => (props.active ? 'flex' : 'none')};
+    margin: 0 auto;
+    height: 3px;
+    width: 72px;
+    background-color: #76acfc;
+  }
+`;
 
 const Tab = ({ name, label, active, onClick }) => (
-  <div>
+  <TabWrapper active={active}>
     <p onClick={() => onClick(name)}>{label}</p>
-  </div>
+  </TabWrapper>
 );
 
 export default Tab;
--- src/features/tabs/TabBar.jsx
+++ src/features/tabs/TabBar.jsx
@@ -4,7 +4,18 @@ import Tab from './Tab';
 
 const TabBarWrapper = styled.div`
   display: flex;
-  border-bottom: 1px solid grey;
+  border-bottom: 1px solid #ececec;
+  position: absolute;
+  left: 0;
+  right: 0;
+`;
+
+const TabBarContent = styled.div`
+  padding-left: 32px;
+  display: flex;
+  width: 1250px;
+  margin: 0 auto;
+  align-items: center;
 `;
 
 const TabBar = props => {
@@ -15,7 +26,11 @@ const TabBar = props => {
     return <Tab key={name} name={name} label={label} active={currentTab === name} onClick={onTabClick} />;
   });
 
-  return <TabBarWrapper>{tabItems}</TabBarWrapper>;
+  return (
+    <TabBarWrapper>
+      <TabBarContent>{tabItems}</TabBarContent>
+    </TabBarWrapper>
+  );
 };
 
 export default TabBar;
