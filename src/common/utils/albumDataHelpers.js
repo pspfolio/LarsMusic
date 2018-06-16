ef51f50b2e7e3765408890f559f029e3f58b85de
Petteri Suominen <pspfolio@gmail.com>
2018-06-16 20:35:01 +0300

    adding album track id list

--- src/common/utils/albumDataHelpers.js
+++ src/common/utils/albumDataHelpers.js
@@ -3,7 +3,8 @@ import mapKeys from 'lodash/mapKeys';
 export const normalizeAlbumData = albums => {
   const normalizedData = albums.map(item => ({
     ...item,
-    artists: item.artists.map(artist => artist.id)
+    artists: item.artists.map(artist => artist.id),
+    tracks: []
   }));
 
   const data = mapKeys(normalizedData, 'id');
