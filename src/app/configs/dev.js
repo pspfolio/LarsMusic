f4bfd81c926fb3d72e1ab7d9f8f82f289c7a168f
Petteri Suominen <pspfolio@gmail.com>
2018-06-26 19:10:31 +0300

    adding firebase config files

---
+++ src/app/configs/dev.js
@@ -0,0 +1,4 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBrygqRh486eayQALusBdr9kSN-pv_ghaY',
+  databaseURL: 'https://larsmusic-20ade.firebaseio.com/'
+};
---
+++ src/app/configs/prod.js
@@ -0,0 +1,5 @@
+export const firebaseConfig = {
+  apiKey: '<Web API Key>',
+  authDomain: '<Project ID>.firebaseapp.com',
+  databaseURL: '<Database url>'
+};
