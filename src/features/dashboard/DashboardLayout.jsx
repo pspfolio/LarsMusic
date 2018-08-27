3528ece74e437fbcabca904ae799289342dba186
Petteri Suominen <pspfolio@gmail.com>
2018-08-27 18:48:37 +0300

    styling sidebar

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import UserName from 'features/user/UserName';
 import Sidebar from 'features/sidebar/Sidebar';
 
 const Content = styled.div`
--- src/features/sidebar/Sidebar.jsx
+++ src/features/sidebar/Sidebar.jsx
@@ -11,10 +11,46 @@ const Aside = styled.aside`
 const Title = styled.h1`
   margin-top: 0;
   padding-top: 50px;
-  font-size: 1.5rem;
+  font-size: 1.5em;
   font-weight: 500;
   text-align: center;
+  letter-spacing: 1.5px;
+  color: #7977fb;
+`;
+
+const NavigationList = styled.ul`
+  margin-top: 48px;
+  padding-left: 0;
+`;
+
+const NavigationListItem = styled.li`
+  margin-left: 60px;
+  margin-bottom: 32px;
+  list-style-type: none;
+`;
+
+const Link = styled(NavLink)`
+  position: relative;
+  text-decoration: none;
+  font-size: 1.1rem;
+  font-weight: 700;
+  color: #3d3333;
   letter-spacing: 1px;
+
+  &::after {
+    position: absolute;
+    content: '';
+    height: 2px;
+    bottom: -8px;
+    left: 0;
+    width: 0;
+    background-color: #7977fb;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover:after {
+    width: 100%;
+  }
 `;
 
 class Sidebar extends Component {
@@ -24,18 +60,18 @@ class Sidebar extends Component {
         <nav>
           <Title>LarsMusic</Title>
 
-          <ul>
-            <li>
-              <NavLink to="/own" activeClassName="selected">
+          <NavigationList>
+            <NavigationListItem>
+              <Link to="/own" activeClassName="selected">
                 Oma musiikki
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/search" activeClassName="selected">
+              </Link>
+            </NavigationListItem>
+            <NavigationListItem>
+              <Link to="/search" activeClassName="selected">
                 Etsi
-              </NavLink>
-            </li>
-          </ul>
+              </Link>
+            </NavigationListItem>
+          </NavigationList>
         </nav>
       </Aside>
     );
