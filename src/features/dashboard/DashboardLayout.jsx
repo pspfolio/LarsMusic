e0f4079966675bdf54ab6d88e82f9f1da35d0fa1
Petteri Suominen <pspfolio@gmail.com>
2018-10-08 22:07:15 +0300

    Adding sidebar close and open

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Sidebar from 'features/sidebar/Sidebar';
+import { devices } from 'common/utils/styledUtils';
+import hamburger from 'assets/images/hamburger.svg';
+
+const NavIcon = styled.img`
+  margin-top: 16px;
+  width: 25px;
+  height: 25px;
+  display: none;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+
+  ${devices.md`
+    display: flex;
+  `};
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
 
 const Content = styled.div`
   display: flex;
@@ -13,11 +32,32 @@ const Container = styled.main`
   height: 100%;
 `;
 
-const DashboardLayout = ({ children }) => (
-  <Content>
-    <Sidebar />
-    <Container>{children}</Container>
-  </Content>
-);
+class DashboardLayout extends Component {
+  state = {
+    mobileNavOpen: false
+  };
+
+  onNavOpenClick = () => {
+    this.setState({ mobileNavOpen: true });
+  };
+
+  onNavCloseClick = () => {
+    this.setState({ mobileNavOpen: false });
+  };
+
+  render() {
+    const { mobileNavOpen } = this.state;
+    const { children } = this.props;
+    return (
+      <Content>
+        <Sidebar mobileNavOpen={mobileNavOpen} onNavCloseClick={this.onNavCloseClick} />
+        <Container>
+          <NavIcon onClick={this.onNavOpenClick} src={hamburger} alt="play" />
+          {children}
+        </Container>
+      </Content>
+    );
+  }
+}
 
 export default DashboardLayout;
--- src/features/sidebar/Sidebar.jsx
+++ src/features/sidebar/Sidebar.jsx
@@ -2,15 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import SiteTitle from 'common/components/SiteTitle';
+import { devices } from 'common/utils/styledUtils';
+import close from 'assets/images/close.svg';
 
 const Aside = styled.aside`
   width: 300px;
   color: #353d33;
   min-height: 100vh;
   background: #fff;
+  transition: left 0.3s ease-in-out;
 
-  @media (max-width: 769px) {
-    display: none;
+  ${devices.md`
+    z-index: 2;
+    position: absolute;
+    left: ${props => (props.navOpen ? '0px' : '-300px')};
+  `};
+`;
+
+const CloseContainer = styled.div`
+  display: none;
+  justify-content: flex-end;
+
+  ${devices.md`
+    display: flex;
+  `};
+`;
+
+const CloseIcon = styled.img`
+  text-align: right;
+  margin-top: 16px;
+  margin-right: 16px;
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.2);
   }
 `;
 
@@ -49,8 +77,11 @@ const Link = styled(NavLink)`
   }
 `;
 
-const Sidebar = () => (
-  <Aside>
+const Sidebar = ({ mobileNavOpen, onNavCloseClick }) => (
+  <Aside navOpen={mobileNavOpen}>
+    <CloseContainer>
+      <CloseIcon src={close} onClick={onNavCloseClick} />
+    </CloseContainer>
     <nav>
       <SiteTitle />
       <NavigationList>
